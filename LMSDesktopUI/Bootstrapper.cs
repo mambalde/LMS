fc4bb0e504ec8266f4bae1869dd48e3dc5223ccb
runtimeterror <55356167+mambalde@users.noreply.github.com>
2021-06-14 23:31:16 +0900

    Desktop UI set up
     Dependency Injection setup for the UI and Keyboard Input bindings setup;

---
+++ LMSDesktopUI/Bootstrapper.cs
@@ -0,0 +1,85 @@
+﻿using Caliburn.Micro;
+using LMSDesktopUI.ViewModels;
+using POSDesktopUI.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LMSDesktopUI
+{
+    public class Bootstrapper:BootstrapperBase
+    {
+        private SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+
+            var defaultCreateTrigger = Parser.CreateTrigger;
+            Parser.CreateTrigger = (target, triggerText) =>
+            {
+                if (triggerText == null)
+                {
+                    return defaultCreateTrigger(target, null);
+                }
+
+                var triggerDetail = triggerText
+                    .Replace("[", string.Empty)
+                    .Replace("]", string.Empty);
+
+                var splits = triggerDetail.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+
+                switch (splits[0])
+                {
+                    case "Key":
+                        var key = (Key)Enum.Parse(typeof(Key), splits[1], true);
+                        return new KeyTrigger { Key = key };
+
+                    case "Gesture":
+                        var mkg = (MultiKeyGesture)(new MultiKeyGestureConverter()).ConvertFrom(splits[1]);
+                        return new KeyTrigger { Modifiers = mkg.KeySequences[0].Modifiers, Key = mkg.KeySequences[0].Keys[0] };
+                }
+
+                return defaultCreateTrigger(target, triggerText);
+            };
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
---
+++ LMSDesktopUI/Input/KeySequence.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace POSDesktopUI.Input
+{
+    public class KeySequence
+    {
+        public Key[] Keys { get; }
+        public ModifierKeys Modifiers { get; }
+        public KeySequence(ModifierKeys modifiers, params Key[] keys)
+        {
+            if (keys == null)
+                throw new ArgumentNullException(nameof(keys));
+
+            if (keys.Length < 1)
+                throw new ArgumentException(@"At least 1 key should be provided", nameof(keys));
+
+            Keys = new Key[keys.Length];
+            keys.CopyTo(Keys, 0);
+            Modifiers = modifiers;
+        }
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            if (Modifiers != ModifierKeys.None)
+            {
+                if ((Modifiers & ModifierKeys.Control) != ModifierKeys.None)
+                    builder.Append("Ctrl+");
+                if ((Modifiers & ModifierKeys.Alt) != ModifierKeys.None)
+                    builder.Append("Alt+");
+                if ((Modifiers & ModifierKeys.Shift) != ModifierKeys.None)
+                    builder.Append("Shift+");
+                if ((Modifiers & ModifierKeys.Windows) != ModifierKeys.None)
+                    builder.Append("Windows+");
+            }
+
+            builder.Append(Keys[0]);
+
+            for (var i = 1; i < Keys.Length; i++)
+            {
+                builder.Append("+" + Keys[i]);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
